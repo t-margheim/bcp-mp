31d7b1c83b3fcaacc72aa89e62466b4b998be5f6
Tim Margheim <timargheim@us.ci.org>
2019-01-18 13:51:44 +0000

    errors on bible retrieval should now be visible

--- pkg/lectionary/bible/bible.go
+++ pkg/lectionary/bible/bible.go
@@ -20,14 +20,27 @@ func (s *Service) GetLesson(reference string) Lesson {
 
 	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf(s.BaseURL, lessonString), nil)
 	if err != nil {
-		log.Println("failed to create request:", err.Error())
+		return Lesson{
+			Reference: "Failed on http.NewRequest()",
+			Body:      template.HTML(fmt.Sprintf("error message: %s", err.Error())),
+		}
 	}
 	req.Header.Add("Authorization", "Token a9a234f364de585a1a6273b00ffe4be9c1b9ab47")
 	httpResponse, err := s.Client.Do(req)
 	if err != nil {
-		log.Println("failed on Do():", err)
+		return Lesson{
+			Reference: "Failed on Client.Do()",
+			Body:      template.HTML(fmt.Sprintf("error message: %s", err.Error())),
+		}
 	}
-	responseBody, _ := ioutil.ReadAll(httpResponse.Body)
+	defer httpResponse.Body.Close()
+	responseBody, err := ioutil.ReadAll(httpResponse.Body)
+	if err != nil {
+		return Lesson{
+			Reference: "Failed on ioutil.ReadAll()",
+			Body:      template.HTML(fmt.Sprintf("error message: %s", err.Error())),
+		}
+	}
 
 	var response resp
 	err = json.Unmarshal(responseBody, &response)
